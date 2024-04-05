279ce81e5df9b1edf7b5c7db739f7989008dd594
JamiulIslamNahin <157962682+JamiulIslamNahin@users.noreply.github.com>
2024-04-06 05:51:43 +0600

    Fix Create Bill controller 3

--- src/Accountant/CreateBillController.java
+++ src/Accountant/CreateBillController.java
@@ -5,13 +5,16 @@
 package Accountant;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import mainpkg.createBill;
 
 /**
  * FXML Controller class
@@ -21,21 +24,35 @@ import javafx.scene.control.TextField;
 public class CreateBillController implements Initializable {
 
     @FXML
-    private ComboBox<?> buyerIdComboBox;
+    private ComboBox<String> buyerIdComboBox;
     @FXML
-    private ComboBox<?> productNameComboBox;
+    private ComboBox<String> productNameComboBox;
     @FXML
     private ComboBox<Integer> quantityComboBox;
     @FXML
     private TextField priceTextFile;
     @FXML
     private DatePicker todayDateDatePicker;
+    @FXML
+    private TextArea showAllTextArea;
+    
+    private ArrayList <createBill> cartList;
 
     /**
      * Initializes the controller class.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    @Override 
+    public void initialize(URL url, ResourceBundle rb) { 
+        cartList = new ArrayList<>(); 
+        productNameComboBox.getItems().addAll(
+               "Nitrotech",
+                "100% Gold Standard Whey Protein"
+                
+                
+                
+                );
+        
+
         // TODO
     }    
 
@@ -49,6 +66,10 @@ public class CreateBillController implements Initializable {
 
     @FXML
     private void addButtonMouseOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void productComboBoxOnSelect(ActionEvent event) {
     }
     
 }
---
+++ src/mainpkg/createBill.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mainpkg;
+
+/**
+ *
+ * @author USER
+ */
+public class createBill {
+    private String productName,buyerId;
+    private float unitPrice; 
+    private int quantity; 
+
+    public createBill(String productName, String buyerId, float unitPrice, int quantity) {
+        this.productName = productName;
+        this.buyerId = buyerId;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+    }
+    
+    
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getBuyerId() {
+        return buyerId;
+    }
+
+    public void setBuyerId(String buyerId) {
+        this.buyerId = buyerId;
+    }
+
+    public float getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    } 
+    
+    public float getTotalAmount(){
+        float totalAmount = (unitPrice*quantity);
+        return totalAmount;
+    
+    }
+
+    @Override
+    public String toString() {
+        return ", buyerId=" + buyerId + "productName=" + productName + ", unitPrice=" + unitPrice +"BDT ,"+" quantity=" + quantity + ",Total Amount="+getTotalAmount()+"BDT";
+    }
+    
+} 
