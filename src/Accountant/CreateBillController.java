5e19ce12e19421176fd6b3631397d19dca277d38
JamiulIslamNahin <157962682+JamiulIslamNahin@users.noreply.github.com>
2024-04-07 00:55:08 +0600

    Fix Create Bill controller update

--- src/Accountant/CreateBillController.java
+++ src/Accountant/CreateBillController.java
@@ -14,7 +14,6 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import mainpkg.createBill;
 
 /**
  * FXML Controller class
@@ -107,7 +106,7 @@ public class CreateBillController implements Initializable {
         
         }
         else{ 
-            createBill tempOBJ = new createBill(productNameComboBox.getValue(), Float.perseFloat(priceTextFile.getText(),quantityComboBox.getValue() ));
+            createBill tempOBJ = new createBill();
         
         }
     }
