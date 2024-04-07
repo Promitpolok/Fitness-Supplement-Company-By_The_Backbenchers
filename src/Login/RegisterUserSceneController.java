1875d4efc53cab88dafc189af007fc97be1f4766
Promit polok <promitchowhan73@gmail.com>
2024-04-07 20:58:45 +0600

    Change Password Scene & Register User Scene fxml & Controller
    New File for Login & Register

---
+++ src/Login/RegisterUserSceneController.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Login;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Promit
+ */
+public class RegisterUserSceneController implements Initializable {
+
+    @FXML
+    private TextField newUserIdTextField;
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private RadioButton maleRadioButton;
+    @FXML
+    private RadioButton femaleRadioButton;
+    @FXML
+    private TextField phoneNumberTextField;
+    @FXML
+    private TextField addressTextField;
+    @FXML
+    private TextField salaryTextField;
+    @FXML
+    private DatePicker dobDatePicker;
+    @FXML
+    private DatePicker dojDatePicker;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void registerButtonOnMouseClick(ActionEvent event) throws IOException {
+        
+        Parent root = null;
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("Login_Interface.fxml"));
+root = (Parent) myLoader.load();
+Scene myScene = new Scene(root);
+
+Login_InterfaceController x = myLoader.getController();
+//x.setValue(value);
+
+Stage myStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+myStage.setScene(myScene);
+myStage.show(); 
+    }
+    
+}
